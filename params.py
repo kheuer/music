4085e80efc326869f6abe8d9cb4c6847576a77e2
kheuer <koos@assheuer.de>
2025-11-19 18:38:30 +0100

    add basic pipeline

--- params.py
+++ params.py
@@ -1,1 +1,9 @@
+# audio parameters
 sample_rate = 22050
+
+# dataset split parameters
+test_size = 0.2
+val_size = 0.2
+train_size = 1 - (test_size + val_size)
+assert round(test_size + val_size + train_size, 6) == 1.0
+assert all(size >= 0 for size in [test_size, val_size, train_size])
