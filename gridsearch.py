dae1f87927809455f8758a223cccd2b83ed0e7fc
Koos Assheuer <koos@assheuer.de>
2026-01-16 11:49:32 +0100

    more results

--- gridsearch.py
+++ gridsearch.py
@@ -12,6 +12,8 @@ from features import (
 from models import (
     pipeline,
     create_simple_feedforward_model,
+    create_complex_feedforward_model,
+    create_residual_cnn_model,
     create_densenet,
     create_resnet,
     create_random_forest,
@@ -19,21 +21,23 @@ from models import (
 )
 from data import df
 
-splits_list = [10, 6, 3, 1]
-batch_sizes = [32, 256]
-learning_rates = [0.0001, 1e-06]
+splits_list = [10, 3]
+batch_sizes = [256]
+learning_rates = [0.0001]
 feature_extractors = [
     compute_spectral_features,
     compute_tempo_features,
-    compute_mel_spectrogram,
-    compute_chromagram,
+    # compute_mel_spectrogram,
+    # compute_chromagram,
 ]
 model_creators = [
     create_random_forest,
     create_svm,
-    create_resnet,
-    create_densenet,
+    # create_resnet,
+    # create_densenet,
     create_simple_feedforward_model,
+    # create_complex_feedforward_model,
+    # create_residual_cnn_model,
 ]
 
 combinations = list(
@@ -109,9 +113,10 @@ for splits, batch_size, lr, feature_extractor, model_creator in tqdm(
         model_creator=model_creator,
         epochs=100,
         batch_size=batch_size,
-        earlystop_patience=10,
+        earlystop_patience=20,
         learning_rate=lr,
         plot_confusion_matrix=False,
+        liveplot_training=False,
     )
 
     results_df = pd.concat(
